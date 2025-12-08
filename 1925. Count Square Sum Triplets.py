adcb5a77aca5745a3d76109caaefb5f0c70ac3d3
Hafiz Muhammad Tariq Raza <muhammadtariq2001@outlook.com>
2025-12-08 19:48:22 +0500

    Add solution for LeetCode 1925 and update removeElement
    Added a new file with a draft solution for '1925. Count Square Sum Triplets'. Refactored '27. Remove Elements.py' to use a more efficient in-place removal algorithm and updated test cases. Also updated project configuration files to use a different Python SDK path.

---
+++ 1925. Count Square Sum Triplets.py
@@ -0,0 +1,9 @@
+
+# def countTriples(n):
+#     count = 0
+#     for i in range( (n+2) - n , n):
+#         if (n-2) ** 2 + (n-1) ** 2 == n**2:
+#             count += 1
+#     return count
+# n = 5
+# print(countTriples(n))
